2c2d141f04e82bc32d38a0936d11a1f816e8c633
Tricide <cameronjspears@icloud.com>
2024-08-30 20:01:41 -0400

    Working SVM model, preparing for comparison, or overnight running

---
+++ SciKitSVM.py
@@ -0,0 +1,52 @@
+from pmlb import fetch_data, classification_dataset_names
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+
+rbf_svm_test_scores = []
+polynomial_svm_test_scores = []
+linear_svm_test_scores = []
+sig_svm_test_scores = []
+
+rbf_svm = svm.SVC(kernel = 'rbf', cache_size = 1000)
+polynomial_svm = svm.SVC(kernel = 'poly', cache_size = 1000)
+linear_svm = svm.SVC(kernel = 'linear', cache_size = 1000)
+sig_svm = svm.SVC(kernel = 'sigmoid', cache_size = 1000)
+
+X,y = fetch_data('banana', return_X_y=True)
+
+train_X, test_X, train_y, test_y = train_test_split(X,y)
+
+rbf_svm.fit(train_X, train_y)
+polynomial_svm.fit(train_X, train_y)
+linear_svm.fit(train_X, train_y)
+
+rbf_svm_test_scores.append(rbf_svm.score(test_X, test_y))
+polynomial_svm_test_scores.append(polynomial_svm.score(test_X, test_y))
+linear_svm_test_scores.append(linear_svm.score(test_X, test_y))
+sig_svm_test_scores.append(sig_svm.score(test_X, test_y))
+'''
+for classification_dataset in classification_dataset_names:
+    
+    rbf_svm = svm.SVC(kernel = 'rbf')
+    polynomial_svm = svm.SVC(kernel = 'poly')
+
+    X,y = fetch_data(classification_dataset, return_X_y=True)
+
+    train_X, test_X, train_y, test_y = train_test_split(X,y)
+
+    rbf_svm.fit(train_X, train_y)
+    polynomial_svm.fit(train_X, train_y)
+
+
+    rbf_svm_test_scores.append(rbf_svm.score(test_X, test_y))
+    polynomial_svm_test_scores.append(polynomial_svm.score(test_X, test_y))
+'''
+
+sb.boxplot(data=[rbf_svm_test_scores, polynomial_svm_test_scores], notch=True)
+plt.xticks([0, 1], ['RBFSVM', 'PolynomialSVM'])
+plt.ylabel('Test Accuracy')
+plt.show()
