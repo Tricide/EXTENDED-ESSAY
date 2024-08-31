b9e4ee6f086271faf519013ec979c5c46bf1ac1d
Tricide <cameronjspears@icloud.com>
2024-08-30 22:23:32 -0400

    I'm almost there. It's too late now though. Sad.

--- SciKitSVM.py
+++ SciKitSVM.py
@@ -5,36 +5,42 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 
 import matplotlib.pyplot as plt
 import seaborn as sb
+##for ease of use
+class supportVector:
+    
+    def __init__(self, kernel_type):
+        self.svm1 = svm.SVC(kernel = kernel_type, cache_size = 1000)
+        self.test_scores = []
+        self.matrices = []
+        self.predictions = []
+    
+    def predictf(self, x, y):
+        self.predictions = self.svm1.predict(x)
+        self.test_scores.append(accuracy_score(self.predictions, y))
+        self.matrices.append(confusion_matrix(y, self.predictions))
+        
+        
+        
+##initiating all necessary variables 
+rbf_svm = supportVector('rbf')
+polynomial_svm = supportVector('poly')
+linear_svm = supportVector('linear')
+sig_svm = supportVector('sigmoid')
 
+supportVectorArray = [rbf_svm, polynomial_svm, linear_svm, sig_svm]
 
-rbf_svm_test_scores = []
-polynomial_svm_test_scores = []
-linear_svm_test_scores = []
-sig_svm_test_scores = []
-
-rbf_svm = svm.SVC(kernel = 'rbf', cache_size = 1000)
-polynomial_svm = svm.SVC(kernel = 'poly', cache_size = 1000)
-linear_svm = svm.SVC(kernel = 'linear', cache_size = 1000)
-sig_svm = svm.SVC(kernel = 'sigmoid', cache_size = 1000)
-
+##preparing the data
 X,y = fetch_data('banana', return_X_y=True)
 
 train_X, test_X, train_y, test_y = train_test_split(X,y)
 
-rbf_svm.fit(train_X, train_y)
-polynomial_svm.fit(train_X, train_y)
-linear_svm.fit(train_X, train_y)
-sig_svm.fit(train_X, train_y)
-
-rbf_predicted=rbf_svm.predict(test_X)
-polynomial_predicted=polynomial_svm.predict(test_X)
-linear_predicted=linear_svm.predict(test_X)
-sig_predicted=sig_svm.predict(test_X)
-
-rbf_svm_test_scores.append(accuracy_score(rbf_predicted))
-polynomial_svm_test_scores.append(accuracy_score(polynomial_predicted))
-linear_svm_test_scores.append(accuracy_score(linear_predicted))
-sig_svm_test_scores.append(accuracy_score(sig_predicted))
+##training the models
+for svmachine in supportVectorArray:
+    svmachine.svm1.fit(train_X, train_y)    
+    
+    ##predict, test, and create confusion matrix
+    svmachine.predictf(test_X, test_y)
+    
 
 '''
 for classification_dataset in classification_dataset_names:
@@ -55,14 +61,10 @@ for classification_dataset in classification_dataset_names:
 '''
 
 
-print(confusion_matrix(train_y, rbf_predicted))
-print(confusion_matrix(train_y, polynomial_predicted))
-print(confusion_matrix(train_y, linear_predicted))
-print(confusion_matrix(train_y, sig_predicted))
 
 
 
-sb.boxplot(data=[rbf_svm_test_scores, polynomial_svm_test_scores, linear_svm_test_scores, sig_svm_test_scores], notch=True)
+sb.boxplot(data=[supportVectorArray[0].test_scores, supportVectorArray[1].test_scores, supportVectorArray[2].test_scores, supportVectorArray[3].test_scores], notch=True)
 plt.xticks([0, 1,2,3], ['RBFSVM', 'PolynomialSVM', 'LinearSVM', 'SigmoidSVM'])
 plt.ylabel('Test Accuracy')
 plt.show()
