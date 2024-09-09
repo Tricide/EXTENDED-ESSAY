e58a98876f6e3637e6f78b225c93a7e4a6682c35
Tricide <cameronjspears@icloud.com>
2024-09-09 05:49:08 -0400

    Got stuck on a polynomial kernel, 3 degrees seems to be a large issue. Will continue running when at home. This is the current data

--- SciKitSVM.py
+++ SciKitSVM.py
@@ -95,14 +95,14 @@ def plotDecisionSurface(X, estimator, y, svm2, dataset):
     ###plot the decision surface
     fig, ax = plt.subplots()
     # title for the plots
-    title = ('Decision surface of linear SVC ')
+    title = ('Decision surface of SVM')
     # Set-up grid for plotting.
     X0, X1 = X[:, 0], X[:, 1]
     xx, yy = make_meshgrid(X0, X1)
     plot_contours(ax, estimator, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
     ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-    ax.set_ylabel('y label here')
-    ax.set_xlabel('x label here')
+    ax.set_ylabel('Feature 1')
+    ax.set_xlabel('Feature 2')
     ax.set_xticks(())
     ax.set_yticks(())
     ax.set_title(title)
@@ -113,7 +113,7 @@ def plotDecisionSurface(X, estimator, y, svm2, dataset):
         plt.savefig("DecisionBoundary/{data}_{model}_{c}_{g}_DecisionBoundary_graph.png".format(data = dataset, model = svm2.svm1.kernel,c = svm2.svm1.C, g =svm2.svm1.gamma))
     if (svm2.svm1.kernel == 'poly'):
         plt.savefig("DecisionBoundary/{data}_{model}_{c}_{g}_{d}_DecisionBoundary_graph.png".format(data = dataset, model = svm2.svm1.kernel,c = svm2.svm1.C, g =svm2.svm1.gamma, d = svm2.svm1.degree))
-
+    plt.clf()
 
 #run model with preset dataset and support vector hyperparameters    
 def run(supportVectorMachine, X, y, dataset):            
@@ -136,46 +136,66 @@ def run(supportVectorMachine, X, y, dataset):
     plotPRCGraph(supportVectorMachine, dataset, test_y)
     
     #Plot Decision Surface
-    plotDecisionSurface(X, estimator, y, supportVectorMachine, dataset)
+    #if (X.ndim == 2):
+    #    plotDecisionSurface(X, estimator, y, supportVectorMachine, dataset)
     
     #open excel sheet
     wb = openpyxl.load_workbook(r"C:\Users\Tricide\Documents\School\EXTENDED ESSAY\Data.xlsx")
     ws = wb.active
     ##append data to excel sheet
-    ws.append((dataset, supportVectorMachine.svm1.kernel, supportVectorMachine.svm1.C, supportVectorMachine.svm1.gamma, supportVectorMachine.svm1.degree, supportVectorMachine.tests["f1score"], roc_auc, "{data}_{model}_roc_graph.png".format(data = dataset, model = supportVectorMachine.svm1.kernel), "{data}_{model}_prc_graph.png".format(data = dataset, model = supportVectorMachine.svm1.kernel),supportVectorMachine.tests["accuracy"], supportVectorMachine.tests["balancedAccuracy"], supportVectorMachine.tests["specificity"], supportVectorMachine.tests["precision"], supportVectorMachine.tests["recall"], ','.join(map(str, supportVectorMachine.matrix)), end-start))
+    ws.append((dataset, supportVectorMachine.svm1.kernel, supportVectorMachine.svm1.C, supportVectorMachine.svm1.gamma, supportVectorMachine.svm1.degree, supportVectorMachine.svm1.coef0, supportVectorMachine.tests["f1score"], roc_auc, "{data}_{model}_roc_graph.png".format(data = dataset, model = supportVectorMachine.svm1.kernel), "{data}_{model}_prc_graph.png".format(data = dataset, model = supportVectorMachine.svm1.kernel),supportVectorMachine.tests["accuracy"], supportVectorMachine.tests["balancedAccuracy"], supportVectorMachine.tests["specificity"], supportVectorMachine.tests["precision"], supportVectorMachine.tests["recall"], ','.join(map(str, supportVectorMachine.matrix)), end-start))
     
     wb.save("Data.xlsx")
+    plt.close('all')
     
 ##all values to be tested        
-datasets = ["banana"] ## "clean2", "parity5",  "GAMETES_Epistasis_2_Way_1000atts_0.4H_EDM_1_EDM_1_1"]       
+datasets = ["banana", "clean2", "parity5",  "GAMETES_Epistasis_2_Way_1000atts_0.4H_EDM_1_EDM_1_1"]       
 sampleAmounts = [100, 1000, 10000]
-features  = [2, 10, 100, 10000]
+features  = [10, 100, 1000]
 informativeFeatures = [.1,.25, .5, .75, 1] ##probably will not be used
 gammaValues = [0.001, 0.01, 0.1, 1, 10]
 cValues = [0.001, 0.01, 0.1, 1, 10]
 degreeValues = [2,3,4]
 shiftValues = [0.001, 0.01, 0.1, 1, 10]
-supportVectorArray = []
 
 
-for amount in sampleAmounts:
-    X_in, y_in = make_classification(n_samples = amount, n_features = 2, n_informative = 2)
-for c in cValues:
-    linear_svm = supportVector('linear', c, 0, 3, 0)
-    run(linear_svm, X_in, y_in, "banana")
-    
-    for gamma in gammaValues:
-        rbf_svm = supportVector('rbf', c, gamma, 3 ,0)
-        run(rbf_svm, X_in, y_in, "banana")
-        
-        for shift in shiftValues:
-            sig_svm = supportVector('sigmoid', c, gamma, 3, shift)
-            run(sig_svm, X_in, y_in, "banana")
+for feature in features:
+    for amount in sampleAmounts:
+        X_in, y_in = make_classification(n_samples = amount, n_features = feature, n_informative = feature, n_redundant = 0)
+        dataset = "synthetic_{amount}_{features}".format(amount = amount, features = feature)
+        for c in cValues:
+            linear_svm = supportVector('linear', c, 0, 3, 0)
+            run(linear_svm, X_in, y_in, dataset)
             
-        for degree in degreeValues:
-            polynomial_svm = supportVector('poly', c, gamma, degree, 0)
-            run(polynomial_svm, X_in, y_in, "banana")
-    
+            for gamma in gammaValues:
+                rbf_svm = supportVector('rbf', c, gamma, 3 ,0)
+                run(rbf_svm, X_in, y_in, dataset)
+                
+                for shift in shiftValues:
+                    sig_svm = supportVector('sigmoid', c, gamma, 3, shift)
+                    run(sig_svm, X_in, y_in, dataset)
+                    
+            for degree in degreeValues:
+                polynomial_svm = supportVector('poly', c, 1, degree, 0)
+                run(polynomial_svm, X_in, y_in, dataset)
+
+for dataset in datasets:
+    x_in, y_in = fetch_data(dataset, return_X_y=True)
+    for c in cValues:
+            linear_svm = supportVector('linear', c, 0, 3, 0)
+            run(linear_svm, X_in, y_in, dataset)
+            
+            for gamma in gammaValues:
+                rbf_svm = supportVector('rbf', c, gamma, 3 ,0)
+                run(rbf_svm, X_in, y_in, dataset)
+                
+                for shift in shiftValues:
+                    sig_svm = supportVector('sigmoid', c, gamma, 3, shift)
+                    run(sig_svm, X_in, y_in, dataset)
+                    
+            for degree in degreeValues:
+                polynomial_svm = supportVector('poly', c, 1, degree, 0)
+                run(polynomial_svm, X_in, y_in, dataset)
 
 
 ##X_in,y_in = fetch_data("banana", return_X_y=True)
